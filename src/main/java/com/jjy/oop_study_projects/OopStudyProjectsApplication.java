cd1c9bc1bb7e5107db49fa2c0a2445feba8a2589
jjy <galid1@naver.com>
2020-01-26 10:20:45 +0900

    init

---
+++ src/main/java/com/jjy/oop_study_projects/OopStudyProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.jjy.oop_study_projects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopStudyProjectsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopStudyProjectsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jjy/oop_study_projects/OopStudyProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjy.oop_study_projects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopStudyProjectsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
